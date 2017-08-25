c46ccf10f15922c413369559ef7fb2ee4b5376a5
Daniel Jacobs <Daniel.Jacobs@austin.utexas.edu>
2017-08-25 14:05:22 -0500

    Added delete and publish methods

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
 
-# Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	posts = Post.objects.order_by('published_date')
@@ -20,7 +19,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -34,7 +32,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -45,3 +42,16 @@ def post_draft_list(request):
 	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
 	return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
